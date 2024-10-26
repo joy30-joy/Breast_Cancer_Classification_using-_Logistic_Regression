453345ca9190fed9019944667a03154e0c6e6a8e
JOY MUKHOPADHYAY <joyba1406@gmail.com>
2024-10-26 11:28:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import numpy as np
-import pandas as pd
 import pickle
 
 # loading model
@@ -9,25 +8,29 @@ model = pickle.load(open('model.pkl', 'rb'))
 # flask app
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Getting the features from the form
     features = request.form['feature']
     features = features.split(',')
+
+    # Converting the features to numpy array
     np_features = np.asarray(features, dtype=np.float32)
 
-    # prediction
+    # Making the prediction
     pred = model.predict(np_features.reshape(1, -1))
-    message = ['Cancrouse' if pred[0] == 1 else 'Not Cancrouse']
-    # print(message[0])
-    return render_template('index.html', message=message)
-
+    
+    # Generating the message based on the prediction
+    message = ['Cancerous' if pred[0] == 1 else 'Not Cancerous']
+    
+    # Rendering the template with the message
+    return render_template('index.html', message=message[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
